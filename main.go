51df70370098b73ab9f88744c76d17ed12241b93
Dania Estrada <ale.estrada99@gmail.com>
2020-01-08 15:22:03 -0600

    testing ssh connection

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import "fmt"
 
 //learned init and main etc.
 // possibly thinking about doing notetaking app but have no idea where to begin
+//testing ssh
 func main() {
 	fmt.Println("I have no idea what my porject is going to be about")
 }
