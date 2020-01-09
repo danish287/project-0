28d643d3a4f2e28e807d3a00369753e44a36d500
Dania Estrada <ale.estrada99@gmail.com>
2020-01-08 19:38:42 -0600

    attempting to get first positionl argument

--- main.go
+++ main.go
@@ -1,12 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+)
 
 //learned init and main etc.
 //1. mvp 2. restructuring
 
-//get user input from user to
+//get user input from user of zodiac sign
+//get user input for love, career, or money
+//get user input for weekly / monthly and yearly
+// default horoscope is daily general horoscope
+
 var zodiac [12]string
+var userZodiac string
 
 func init() {
 	zodiac = [12]string{
@@ -24,8 +31,13 @@ func init() {
 		"capricorn",
 	}
 
+	//flag.String(&userZodiac, "", "get user zodiac")
+	temp := flag.Arg(0)
+	//flag.Parse()
+	println("SUER ZODIAC", temp)
+
 }
 
 func main() {
-	fmt.Println(zodiac[0])
+	//fmt.Println(zodiac[0])
 }
