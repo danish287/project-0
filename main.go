9b51acfa8813e1b812fb88fec74a209dfe482794
Dania Estrada <ale.estrada99@gmail.com>
2020-01-09 18:21:12 -0600

    testing package

--- main.go
+++ main.go
@@ -1,9 +1,8 @@
 package main
 
-import (
-	"flag"
-	//"github.com/danish287/project-0/shaker"
-)
+import "fmt"
+
+//"github.com/danish287/project-0/shaker"
 
 //learned init and main etc.
 //1. mvp 2. restructuring hello
@@ -13,21 +12,6 @@ import (
 //get user input for weekly / monthly and yearly
 // default horoscope is daily general horoscope
 
-var zodiac [12]string
-var userZodiac string
-
-func init() {
-	// zodiac = [12]string{
-
-	// }
-
-	//flag.String(&userZodiac, "", "get user zodiac")
-	temp := flag.Arg(0)
-	//flag.Parse()
-	println("SUER ZODIAC", temp)
-
-}
-
 func main() {
-	//fmt.Println(zodiac[0])
+	fmt.Println(PrintZodiac(zodiacSigns))
 }
