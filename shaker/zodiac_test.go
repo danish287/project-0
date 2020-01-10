3eb17fd2e70f6a101548872b89ad3e0a07d139f9
Dania Estrada <ale.estrada99@gmail.com>
2020-01-09 19:04:42 -0600

    Included working unit tests

--- shaker/zodiac_test.go
+++ shaker/zodiac_test.go
@@ -1,1 +1,13 @@
 package shaker
+
+import "testing"
+
+func TestPrintZodiac(t *testing.T) {
+	zodiacSigns := []string{"aquarius", "pices", "aries"}
+	n := PrintZodiac(zodiacSigns)
+
+	if len(n) == 0 {
+		t.Error("No sign")
+	}
+
+}
