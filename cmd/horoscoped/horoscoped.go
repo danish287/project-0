dcb18eec652807bf6fbf4439c1c651075c1f35d2
danish287 <ale.estrada99@gmail.com>
2020-01-22 07:27:09 -0600

    trying to get index.html up

--- cmd/horoscoped/horoscoped.go
+++ cmd/horoscoped/horoscoped.go
@@ -3,32 +3,34 @@ package main
 import (
 	"fmt"
 	"net/http"
-
-	"github.com/danish287/project-0/config"
-	"github.com/danish287/project-0/shaker"
 )
 
+// var tpl *template.Template
+
+// func init() {
+// 	tpl = template.Must(template.ParseGlob("../../web/*.html"))
+// }
+
 func main() {
-	// userIn := config.ZodiacSign
-
-	// if userIn == "EMPTY" {
-	// 	fmt.Println("\nPlease enter a zodiac sign using the flag Zodiac. \nExample: -Zodiac=Aquarius")
-	// }
-
-	// if zodiacURL == "Please try again using valid arguments." {
-	// 	fmt.Println("Please try again using valid arguments.")
-	// } else {
-
 	println("Server is running on port 8080")
 
 	http.Handle("/", http.FileServer(http.Dir("web")))
+	//http.Handle("/", http.FileServer("../../web/*.html"))
+	//"../../web/*.html"
 	http.HandleFunc("/horoscope", func(w http.ResponseWriter, r *http.Request) {
-			var myZodiac = r.FormValue("ZodiacSign")
-			zodiacURL := shaker.GetZodiacURL(userIn, config.ReadingFor, config.ReadingType)
-			fmt.Fprint(w, shaker.GetMyResponse(zodiacURL, config.ReadingFor), " ", name)
-		})
-		http.ListenAndServe(":8080", nil)
-		fmt.Println("\n\n ", shaker.GetMyResponse(zodiacURL, config.ReadingFor))
-	}
+		var name = r.FormValue("name")
+		if name == "" {
+			name = "Anonymous NAME"
+		}
+		fmt.Fprint(w, name)
+	})
+	http.ListenAndServe(":8080", nil)
 
+	//http.ListenAndServe(":8080", nil)
 }
+
+// func index(w http.ResponseWriter, r *http.Request) {
+// 	//io.WriteString(w, "DANIA")
+// 	tpl.ExecuteTemplate(w, "index.html", nil)
+
+// }
